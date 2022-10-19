36beb7bad2bc781736b0363e6910ca7a1c4970fe
Иван Сафронов <safroniv@mail.ru>
2022-10-19 22:09:50 +0300

    Add project files.

---
+++ EmployeeService/Controllers/DepartmentController.cs
@@ -0,0 +1,59 @@
+﻿using EmployeeService.Data;
+using EmployeeService.Models.Dto;
+using EmployeeService.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeService.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        #region Services 
+
+        private readonly IDepartmentRepository _departmentRepository;
+
+        #endregion
+
+        #region Constructors
+
+        public DepartmentController(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+
+        #endregion
+
+        [HttpGet("departments/all")]
+        public ActionResult<IList<DepartmentDto>> GetAllDepartments()
+        {
+            return Ok(_departmentRepository.GetAll().Select(dp =>
+                new DepartmentDto
+                {
+                    DepartmentId = dp.Id,
+                    FirstName = dp.Description
+                }
+                ).ToList());
+        }
+
+
+        [HttpPost("departments/create")]
+        public ActionResult<int> CreateDepartment([FromQuery] string description)
+        {
+            return Ok(_departmentRepository.Create(new Department
+            {
+                Description = description
+            }));
+        }
+
+        [HttpDelete("departments/delete")]
+        public ActionResult<bool> DeleteDepartment([FromQuery] Guid id)
+        {
+            return Ok(_departmentRepository.Delete(id));
+        }
+
+    }
+}
